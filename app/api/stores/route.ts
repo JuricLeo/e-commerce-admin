8b5d548aa584a1e7db012ec71899c6730cc68921
JuricLeo <ljurickc@gmail.com>
2023-11-19 20:52:08 +0100

    First commit

---
+++ app/api/stores/route.ts
@@ -0,0 +1,33 @@
+import db from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const { name } = body;
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    const store = await db.store.create({
+      data: {
+        name,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORES_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
